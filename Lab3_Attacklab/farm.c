72c3da5e51ef170c342f311f47154fcad1be7a37
ChoYune <fusma159@naver.com>
2022-11-04 23:16:30 +0900

    add attacklab

---
+++ Lab3_Attacklab/farm.c
@@ -0,0 +1,223 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+void setval_383(unsigned *p)
+{
+    *p = 3347663031U;
+}
+
+void setval_127(unsigned *p)
+{
+    *p = 2421721931U;
+}
+
+unsigned getval_172()
+{
+    return 3281017008U;
+}
+
+void setval_236(unsigned *p)
+{
+    *p = 3264239702U;
+}
+
+void setval_298(unsigned *p)
+{
+    *p = 3351742792U;
+}
+
+void setval_124(unsigned *p)
+{
+    *p = 2429012296U;
+}
+
+unsigned getval_162()
+{
+    return 2428995912U;
+}
+
+unsigned getval_119()
+{
+    return 2425641049U;
+}
+
+/* This function marks the middle of the farm */
+int mid_farm()
+{
+    return 1;
+}
+
+/* Add two arguments */
+long add_xy(long x, long y)
+{
+    return x+y;
+}
+
+unsigned getval_277()
+{
+    return 2425411208U;
+}
+
+unsigned getval_453()
+{
+    return 3525888649U;
+}
+
+void setval_443(unsigned *p)
+{
+    *p = 2529416873U;
+}
+
+unsigned getval_410()
+{
+    return 2430634304U;
+}
+
+void setval_135(unsigned *p)
+{
+    *p = 3224950409U;
+}
+
+unsigned addval_475(unsigned x)
+{
+    return x + 3286272328U;
+}
+
+void setval_351(unsigned *p)
+{
+    *p = 3374891401U;
+}
+
+void setval_249(unsigned *p)
+{
+    *p = 2425408153U;
+}
+
+unsigned addval_363(unsigned x)
+{
+    return x + 3525362073U;
+}
+
+unsigned addval_486(unsigned x)
+{
+    return x + 3223374473U;
+}
+
+unsigned addval_100(unsigned x)
+{
+    return x + 3284240769U;
+}
+
+void setval_456(unsigned *p)
+{
+    *p = 3252717896U;
+}
+
+unsigned addval_314(unsigned x)
+{
+    return x + 2430632264U;
+}
+
+unsigned addval_217(unsigned x)
+{
+    return x + 3286272328U;
+}
+
+unsigned addval_489(unsigned x)
+{
+    return x + 3767091373U;
+}
+
+unsigned getval_175()
+{
+    return 3247489417U;
+}
+
+unsigned addval_498(unsigned x)
+{
+    return x + 3372799625U;
+}
+
+unsigned getval_117()
+{
+    return 3281046153U;
+}
+
+unsigned addval_198(unsigned x)
+{
+    return x + 3222853257U;
+}
+
+unsigned addval_451(unsigned x)
+{
+    return x + 3375939981U;
+}
+
+unsigned getval_167()
+{
+    return 3234122377U;
+}
+
+unsigned getval_290()
+{
+    return 3525364365U;
+}
+
+unsigned getval_491()
+{
+    return 3284240435U;
+}
+
+unsigned getval_354()
+{
+    return 2425539209U;
+}
+
+void setval_136(unsigned *p)
+{
+    *p = 3286272456U;
+}
+
+unsigned getval_372()
+{
+    return 3269495112U;
+}
+
+unsigned getval_257()
+{
+    return 3247035180U;
+}
+
+unsigned getval_197()
+{
+    return 3526940313U;
+}
+
+unsigned getval_300()
+{
+    return 3675838089U;
+}
+
+unsigned addval_386(unsigned x)
+{
+    return x + 3375945352U;
+}
+
+unsigned getval_336()
+{
+    return 3525888649U;
+}
+
+unsigned addval_274(unsigned x)
+{
+    return x + 3372794241U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
---
+++ Lab3_Attacklab/phase2.s
@@ -0,0 +1,5 @@
+movq $0x5c6b2b26, %rdi
+pushq $0x0
+movl $0x55555e7e, (%rsp)
+movl $0x00005555, 4(%rsp)
+ret
